2a55a78d8b3a6dc9e3aaf6beb03ae19fc4fb20a2
Sam <armandawoodkhan@gmail.com>
2025-06-18 03:58:22 +0530

    style: Improve layout and styling of Signup component

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+
+import Input from '../components/Input'
+import Button from '../components/Button'
+import { Link } from 'react-router-dom'
 
 export default function Signup() {
   return (
     <div className='h-screen w-screen bg-[#808080] flex justify-center items-center'>
-        <div className='w-3/10 h-fit bg-white flex flex-col gap-2'>
-            <div>
-                <h1>Sign Up</h1>
-                <p>Enter Your Information to create an account</p>
+        <div className='w-1/4 h-fit bg-white flex flex-col gap-3 p-6 rounded-md'>
+            <div className='text-center'>
+                <h1 className='text-4xl font-bold'>Sign Up</h1>
+                <p className=' p-3'>Enter Your Information to create an account</p>
             </div>
+            <Input label="First Name" type="text" placeholder="Jhon"/>
+            <Input label="Last Name" type="text" placeholder="Doe"/>
+            <Input label="Username" type="text" placeholder="DoeJhon#143"/>
+            <Input label="Password" type="password" placeholder="Enter Your Password"/>
+
+
+            <Button text="Sign Up" className="mt-2"/>
+
+            <div className='text-[14px] text-center font-semibold'>Already have an account? <Link to={'/signin'} className='underline'>Login</Link></div>
         </div>
 
     </div>
