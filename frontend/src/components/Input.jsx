c14108780b547e96ef3df7bb26b66e8231d55667
Sam <armandawoodkhan@gmail.com>
2025-06-19 04:26:43 +0530

    feat: Add onChange handler to Input component; update Dashboard to filter users based on input

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,9 +1,9 @@
 
-export default function Input({label,type,ref,placeholder,className}) {
+export default function Input({label,type,ref,placeholder,className,onChange}) {
   return (
     <div className="flex flex-col gap-2 ">
         <label htmlFor="" className="font-semibold">{label}</label>
-        <input className={`outline-1 outline-gray-300 p-2 rounded-md focus:outline-gray-900 focus:outline-2 ${className}`} ref={ref} type={type} placeholder={placeholder} />
+        <input onChange={onChange} className={`outline-1 outline-gray-300 p-2 rounded-md focus:outline-gray-900 focus:outline-2 ${className}`} ref={ref} type={type} placeholder={placeholder} />
     </div>
   )
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Navbar from '../components/Navbar'
 import Input from '../components/Input'
 import ProfileImage from '../components/ProfileImage'
@@ -10,7 +10,7 @@ function Dashboard() {
     const [users, setusers] = useState([])
     const [balance,setBalance] = useState(0)
     const [fullName,setFullName] = useState("User")
-    const [filter,setFilter] = useState(" ")
+    const [filter,setFilter] = useState("")
     const navigate = useNavigate();
     
     useEffect(() => {
@@ -23,20 +23,28 @@ function Dashboard() {
             setBalance(res.data.data.balance);
             setFullName(res.data.data.userId.firstName+" "+res.data.data.userId.lastName);
         }
-        const fetchUserList = async() =>{
-            const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/user/bulk?filter=${filter}`,{
-                headers:{
-                    Authorization: `Bearer ${localStorage.getItem("token")}`
-                }
-            })
-            setusers(res.data.data)
-        }
+       
       fetchUserData();
-      fetchUserList();
 
     }, [])
     
-
+    useEffect(() => {
+       const fetchUserList = async() =>{
+        console.log(filter);
+        
+            const timeOut = setTimeout(async ()=>{
+                await axios.get(`${import.meta.env.VITE_BACKEND_URL}/user/bulk?filter=${filter}`,{
+                headers:{
+                    Authorization: `Bearer ${localStorage.getItem("token")}`
+                }
+                }).then(res=>{
+                    setusers(res.data.data)
+                })
+            },500)
+        }
+      fetchUserList();
+    }, [filter])
+    
   return (
     <div className=''>
         <Navbar fullName={fullName}/>
@@ -44,7 +52,11 @@ function Dashboard() {
             <div className='text-2xl font-medium'>Your Balance: ${balance}</div>
             <div className='flex flex-col gap-2'>
                 <h2 className='text-2xl font-medium'>Users</h2>
-                <Input type="text" placeholder="Search users..."></Input>
+                <Input onChange={(e)=>{
+                    setFilter(e.target.value)
+                    console.log(filter);
+                    
+                }} type="text" placeholder="Search users..."></Input>
             </div>
             <div className='flex flex-col gap-2'>
                 {users.map(user=>(
