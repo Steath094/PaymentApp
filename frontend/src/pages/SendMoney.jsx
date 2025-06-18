c0b549f5f44d795a03a5afa0912120f361b34062
Sam <armandawoodkhan@gmail.com>
2025-06-19 04:02:28 +0530

    feat: Implement Send Money functionality with transfer handling; update UI to display recipient details and amount input

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Input from '../components/Input'
 import Button from '../components/Button'
 import ProfileImage from '../components/ProfileImage'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import axios from 'axios'
 
 export default function SendMoney() {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("id");
+  const fullName = searchParams.get("name").replace("%20"," ");
+  const amountRef = useRef();
+  const navigate = useNavigate();
+  const handleTransfer = async () =>{
+    const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/account/transfer`,
+      {
+        to: id,
+        amount: amountRef.current.value
+      },
+      {
+      headers:{
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+      }
+    })
+    console.log(response);
+    
+    if (response.data.success) {
+      alert("Transfer Successfull");
+      navigate('/')
+    }
+  }
   return (
      <div className='h-screen w-screen bg-[#808080] flex justify-center items-center'>
         <div className='w-1/4 h-fit bg-white flex flex-col gap-3 p-6 rounded-md'>
@@ -12,12 +37,12 @@ export default function SendMoney() {
             </div>
 
             <div className='flex items-center gap-4 mt-8'>
-              <ProfileImage fullName="Friend's Name" variant='Secondary'/>
-              <h1 className='text-2xl font-semibold'>Friend's Name</h1>
+              <ProfileImage fullName={fullName} variant='Secondary'/>
+              <h1 className='text-2xl font-semibold'>{fullName}</h1>
             </div>
-            <Input label="Amount (in Rs)" type="number" placeholder="Enter Amount" className=''/>
+            <Input ref={amountRef} label="Amount (in Rs)" type="number" placeholder="Enter Amount" className=''/>
 
-            <Button text="Initiate Transfer" className="mt-2 bg-[#22c45e] hover:text-[#22c45e] "/>
+            <Button onClick={handleTransfer} text="Initiate Transfer" className="mt-2 bg-[#22c45e] hover:text-[#22c45e] "/>
         </div>
 
     </div>
